9ddc1cf40336ebe728539c0eb9b0938ea344edf0
Shiplu Mokaddim <shiplu.net@gmail.com>
2013-01-04 20:01:46 +0600

    shortened by aliasing namespace

--- example.php
+++ example.php
@@ -1,15 +1,15 @@
 <?php
 require 'datagenerator.php';
 
-use \RandomDataGenerator\*;
+use \RandomDataGenerator as RDG;
 
 
-$dgen = new DataGenerator();
-$dgen->addField('username' , new LowerCaseAlphabetField(6, 12));
-$dgen->addField('age' , new IntegerField(20, 31));
-$dgen->addField('sex' , new SetField(array('Male', 'Female', 'Transexual')));
-$dgen->addField('weight' , new DoubleField(50, 80, 1));
-$dgen->addField('source', new SetField(array('Facebook', 'Google+', 'Twitter')));
+$dgen = new RDG\DataGenerator();
+$dgen->addField('username' , new RDG\LowerCaseAlphabetField(6, 12));
+$dgen->addField('age' , new RDG\IntegerField(20, 31));
+$dgen->addField('sex' , new RDG\SetField(array('Male', 'Female', 'Transexual')));
+$dgen->addField('weight' , new RDG\DoubleField(50, 80, 1));
+$dgen->addField('source', new RDG\SetField(array('Facebook', 'Google+', 'Twitter')));
 
 $data = $dgen->generate(2);
 
